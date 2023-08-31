e52b60a55a90adb8738067cc189a08e99fbe4221
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-08-31 14:16:04 +0500

    project setup

--- app/src/main/java/com/example/calmmind/presentation/MainActivity.kt
+++ app/src/main/java/com/example/calmmind/presentation/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.calmmind
+package com.example.calmmind.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calmmind.R
+import com.example.calmmind.utilits.APP_ACTIVITY
+import com.example.calmmind.utilits.setStatusBarGradiant
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        APP_ACTIVITY = this
+        setStatusBarGradiant(this)
     }
 }
